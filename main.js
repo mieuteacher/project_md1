343cea2f5a12767b7c1098210562766503b0381f
NTBPhuoc <mieuteacher@gmail.com>
2024-06-22 21:38:47 +0700

    finish login logout authentication

---
+++ main.js
@@ -0,0 +1,14 @@
+// let userList = [
+//     {
+//         id: Date.now(),
+//         userName: "admin",
+//         password: "123"
+//     },
+//     {
+//         id: Date.now(),
+//         userName: "member1",
+//         password: "1234"
+//     }
+// ]
+
+// localStorage.setItem("userList", JSON.stringify(userList))
